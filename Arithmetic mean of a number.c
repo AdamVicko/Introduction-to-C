edf8db6992a9fc7af303daffdbc01605213f7e17
AdamVicko <vickovic2203@gmail.com>
2022-01-14 20:42:16 +0100

    Create Arithmetic mean of a number.c

---
+++ Arithmetic mean of a number.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int zbroj,broj1,broj2;
+float arts;
+int main()
+{
+    scanf("%d%d",&broj1,&broj2);
+    zbroj=broj1+broj2;
+    arts=zbroj/2;
+    printf("Aritmeticka sredina je:%f",arts);
+}
